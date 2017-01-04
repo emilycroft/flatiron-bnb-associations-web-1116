9c3d8bc07df9bf280d5779e03d217f5030cea5ce
emilycroft <emilyhcroft@gmail.com>
2017-01-04 13:47:42 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,4 @@ class User < ActiveRecord::Base
   has_many :trips, class_name: 'Reservation', foreign_key: 'guest_id'
   has_many :reviews, :foreign_key => 'guest_id'
   has_many :reservations, through: :listings
-
-
-
-
-  # def trips
-  #   # Reservation.create(guest_id: self.id)
-  #   # binding.pry
-  #
-  #   self.reservations.collect do |trip|
-  #     trip
-  #   end
-  # end
 end
